ea84ad9d5250c32f12b698a84ab2c7dbf0947130
priyadharsini <priyadharshini4738@gmail.com>
2026-04-16 03:41:26 +0530

    commit

--- Task4 codem/Task10/node.js
+++
@@ -1,6 +0,0 @@
-console.log("Sync");
-process.nextTick(()=>console.log("nextTick"));
-Promise.resolve().then(()=>console.log("Promise"));
-setTimeout(()=>console.log("setTimeout"), 0);
-setImmediate(()=>console.log("setImmediate"));
-require('fs').readFile(__filename,()=>console.log("I/O"));
